976d0ece9aa6e0c75fbe9c40ab6b6b9cda9f0174
Leonid Shevchuk <l.shevchuk@corp.vk.com>
2023-09-19 11:51:00 +0300

    Инициализация мини-приложения через VKWebAppInit

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
+import vkBridge from '@vkontakte/vk-bridge';
+
+vkBridge.send('VKWebAppInit');
 
 const container = document.getElementById('root');
 
